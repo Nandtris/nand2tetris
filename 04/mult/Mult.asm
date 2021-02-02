ffa9bacf9a49b3b274b13fe384a6ceeedff5443a
guanshan007 <guanshan007@outlook.com>
2021-02-02 22:19:51 +0800

    completed 04~fill-mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R2
+M=0
+@i
+M=1
+(LOOP)
+    @i
+    D=M
+    @R0
+    D=D-M
+    @END
+    D;JGT  // if i>R0 goto END
+
+    @R1
+    D=M
+    @R2
+    M=D+M  // R2=R1+R1+...+R1    
+
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
